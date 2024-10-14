4426acf793497c3764cf8000022adc23dc1d0ab2
Parker Montgomery <peejmontyw@gmail.com>
2024-10-14 11:48:54 -0600

    Add files via upload

---
+++ src/arrays.js
@@ -0,0 +1,125 @@
+// Given an array of numbers, return all the numbers that are greater than ten.
+// Ex.:
+//   greaterThanTen([1, 2, 3, 11, 12, 13]);
+//   => [11, 12, 13]
+
+function greaterThanTen(numbers) {
+  return numbers.filter(number => number > 10);
+}
+
+
+// Given an array of strings, return all words that start with 'b' or 'B'.
+// Ex.:
+//   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
+//   => ['banana', 'Bonobo]
+
+function bWords(words) {
+  return words.filter(word => word.toLowerCase().startsWith('b'));
+}
+
+// Add all the elements from additionalItems to the end of originalArray.
+// Return the originalArray..
+// Ex.:
+//  extend([1, 2, 3], [4, 5, 6]);
+//  => [1, 2, 3, 4, 5, 6]
+
+
+function extend(originalArray, additionalItems) {
+  originalArray.push(...additionalItems);
+  return originalArray;
+}
+
+
+// Return an array of all items with the given length.
+// Ex.:
+//   itemsWithLength(['a', 'bbb', 'cccc', 'dddddd', 'eee'], 3);
+//   => ['bbb', 'eee']
+
+
+function itemsWithLength(items, length) {
+  return items.filter(item => item.length === length);
+}
+
+
+// Return an array with every other element from the input array (start with index 0).
+// Ex.:
+//   everyOtherItem(['a', 'b', 'c', 'd', 'e']);
+//   => ['a', 'c', 'e']
+
+function everyOtherItem(items) {
+  return items.filter((items, index) => index % 2 === 0);
+}
+
+// Given a list of words and a letter, return the indexes of the words that
+// start with that letter. You can assume that the words and letter will always
+// be lowercased.
+// Ex.:
+//   findWordsStartingWith(['apple', 'banana', 'kiwi', 'pear', 'bacon'], 'b');
+//   => [1, 4]
+
+function findWordsStartingWith(words, letter) {
+  return words.filter(word => word.startsWith(letter));
+}
+
+// Return the `n` smallest values in the array in descending order (largest
+// numbers first). Assume that `n` will always be less than the length of the
+// array.
+// Ex.:
+//   smallestNItems([1, 30, 4, 21, 100000], 3);
+//   => [21, 4, 1]
+
+function smallestNItems(items, n) {
+  items.sort((a, b) => a - b);
+
+let smallestItems = items.slice(0, n);
+smallestItems.reverse();
+
+return smallestItems;
+}
+
+
+
+// Search for a value in the array and return its index. If the value appears
+// more than once, return the index of the *FIRST* occurrence of the value. If
+// the value doesn't exist in the array, return undefined.
+// Ex.:
+//   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
+//   => 2
+function findIndex(items, value) {
+  let index = items.indexOf('c');
+
+  if (index === -1) {
+    return undefined;
+  }
+  return index;
+}
+
+// Given a start number and stop number, return a new array containing all the numbers
+// between the start and stop number.
+// Ex.:
+//   range(1, 5);
+//   => [1, 2, 3, 4, 5]
+function range(start, stop) {
+  let results = []
+
+  for (let i = start; i <= stop; i++) {
+    results.push(i);
+}
+
+return results;
+
+}
+
+// make this look nicer eventually :)
+
+export {
+  bWords,
+  everyOtherItem,
+  extend,
+  findIndex,
+  findWordsStartingWith,
+  greaterThanTen,
+  itemsWithLength,
+  range,
+  smallestNItems,
+};
